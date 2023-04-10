1be1a0f9722bda752a3574d0d80df7983b0b139c
Denx <25338291+denx20@users.noreply.github.com>
2023-04-10 15:29:22 -0400

    Finalize arithmetic test

--- Tests/arithmetic_test_gen.hs
+++ Tests/arithmetic_test_gen.hs
@@ -3,19 +3,19 @@
 
 import System.Random (randomRIO)
 import Control.Monad (replicateM)
-import Data.Text (Text, pack, unpack)
+import Data.Text (Text, pack, unpack, intercalate)
+import Data.String (fromString)
 import Data.Text.IO (writeFile)
 import Arithmetic
 
 randomFunctionName :: IO String
 randomFunctionName = do
-    i <- randomRIO (0, 4) :: IO Int
+    i <- randomRIO (0, 3) :: IO Int
     return $ case i of
         0 -> "add"
         1 -> "multiply"
         2 -> "subtract"
         3 -> "divide"
-        4 -> "exponentiate"
 
 randomArgs :: Int -> IO [Double]
 randomArgs n = replicateM n (randomRIO (-10, 10))
@@ -27,11 +27,15 @@ randomFunctionCall = do
     let argsText = map (pack . show) args
     return $ pack (functionName ++ " ") <> mconcat (map (\a -> "(" <> a <> ")" <> " ") argsText)
 
+getNVarNames :: Int -> Text -> [Text]
+getNVarNames n prefix = map (\i -> prefix <> fromString (show i)) [1..n]
+
 main :: IO ()
 main = do
     putStrLn "Enter the number of lines (N):"
     n <- readLn :: IO Int
+    let tmps = getNVarNames n "tmp"
     functionCalls <- replicateM n randomFunctionCall
-    let programText = "import Arithmetic\nimport Prelude hiding (subtract)\nimport System.CPUTime\nimport Text.Printf\n\nmain :: IO ()\nmain = do\n  start <- getCPUTime\n" <> mconcat (map (\call -> "  let _ = " <> call <> "\n") functionCalls) <> "  end <- getCPUTime\n  let diff = fromIntegral (end - start) / (10 ^ 12 :: Double)\n  printf \"Execution time: %0.6f sec\\n\" (diff :: Double)\n"
+    let programText = "import Arithmetic\nimport Prelude hiding (subtract)\nimport System.CPUTime\nimport Text.Printf\n\nmain :: IO ()\nmain = do\n  start <- getCPUTime\n" <> mconcat (map (\(i, call) -> "  let " <> tmps !! i <> " = " <> call <> "\n") $ zip [0..] functionCalls) <> "  let result = sum [" <> intercalate ", " tmps <> "]\n  print result\n  end <- getCPUTime\n  let diff = fromIntegral (end - start) / (10 ^ 12 :: Double)\n  printf \"Execution time: %0.6f sec\\n\" (diff :: Double)\n"
     Data.Text.IO.writeFile "Tests/arithmetic_test.hs" programText
     putStrLn "arithmetic_test.hs has been generated."
