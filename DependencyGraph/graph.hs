5e5b3f7c6a4c07b86ed0fa5b90d09105a128cd2b
Denx <25338291+denx20@users.noreply.github.com>
2023-04-11 21:42:43 -0400

    Add identifies for var and func and make func names unique

--- DependencyGraph/graph.hs
+++ DependencyGraph/graph.hs
@@ -1,11 +1,12 @@
 {-# LANGUAGE LambdaCase #-}
-module DependencyGraph.Graph(main) where
+module Graph(main) where
 
 import Language.Haskell.Exts
 import System.Environment
 import Data.Maybe
 import Data.List
 import Debug.Trace
+import Control.Monad.State
 
 main :: IO ()
 main = do
@@ -14,7 +15,7 @@ main = do
         [fileName] -> do
             content <- readFile fileName
             case parseModule content of
-                ParseOk ast -> putStr $ showDataDependencies $ extractDataDependencies ast
+                ParseOk ast -> putStr $ showDataDependencies $ evalState (extractDataDependencies ast) 0
                 ParseFailed _ errMsg -> error errMsg
         _ -> error "Usage: dependency-graph <file>"
 
@@ -23,16 +24,19 @@ type DependencyGraph = [(String, [String])]
 showDataDependencies :: DependencyGraph -> String
 showDataDependencies = unlines . map (\(v, fs) -> "(" ++ show v ++ ", " ++ show fs ++ ")")
 
-extractDataDependencies :: Module SrcSpanInfo -> DependencyGraph
-extractDataDependencies (Module _ _ _ _ decls) =
+extractDataDependencies :: Module SrcSpanInfo -> State Int DependencyGraph
+extractDataDependencies (Module _ _ _ _ decls) = do
     let mainDecls = fromMaybe (error "No main function found") $ findMainFunction decls
         binds = collectBinds mainDecls
         vars = [v | PatBind _ (PVar _ (Ident _ v)) _ _ <- binds]
-    in concatMap (\case
-                    (PatBind _ (PVar _ (Ident _ var)) (UnGuardedRhs _ expr) _)
-                        -> let funcCalls = collectFunctionCalls vars expr
-                        in [(var, [name]) | (name, _) <- funcCalls] ++ funcCalls
-                    _ -> []) binds
+    fmap concat . forM binds $ \case
+                    (PatBind _ (PVar _ (Ident _ var)) (UnGuardedRhs _ expr) _) -> do
+                        funcCalls <- collectFunctionCalls vars expr
+                        uniqueFuncCalls <- forM funcCalls $ \(name, args) -> do
+                            return ("f_" ++ name, args)
+                        return $ [("v_" ++ var, [name]) | (name, _) <- uniqueFuncCalls] ++ uniqueFuncCalls
+                    _ -> return []
+
   where
     findMainFunction = find (\case
                             PatBind _ (PVar _ (Ident _ name)) _ _ -> name == "main"
@@ -44,16 +48,24 @@ extractDataDependencies (Module _ _ _ _ decls) =
     collectPatBinds (LetStmt _ (BDecls _ decls)) = decls
     collectPatBinds _ = []
 
-collectFunctionCalls :: [String] -> Exp SrcSpanInfo -> [(String, [String])]
-collectFunctionCalls vars expr = nub $ collect expr
+collectFunctionCalls :: [String] -> Exp SrcSpanInfo -> State Int [(String, [String])]
+collectFunctionCalls vars expr = do
+    result <- collect expr
+    return $ nub result
   where
     collect e = case e of
-                  (App _ e1 e2) ->
+                  (App _ e1 e2) -> do
                       let (baseFunc, args) = extractFuncAndArgs e
-                      in collectApp baseFunc args ++ concatMap collect args
-                  (InfixApp _ e1 op e2) -> collect e1 ++ collectOp op e1 e2 ++ collect e2
+                      appResult <- collectApp baseFunc args
+                      rest <- mapM collect args
+                      return $ appResult ++ concat rest
+                  (InfixApp _ e1 op e2) -> do
+                      c1 <- collect e1
+                      cop <- collectOp op e1 e2
+                      c2 <- collect e2
+                      return $ c1 ++ cop ++ c2
                   (Paren _ e1) -> collect e1
-                  _ -> []
+                  _ -> return []
 
     extractFuncAndArgs (App _ e1 e2) =
         let (f, args) = extractFuncAndArgs e1
@@ -61,16 +73,28 @@ collectFunctionCalls vars expr = nub $ collect expr
     extractFuncAndArgs e = (e, [])
 
     collectApp (Var _ (UnQual _ (Ident _ s))) args
-      | s `notElem` vars = [(s, concatMap collectArgs args)]
-    collectApp _ _ = []
+      | s `notElem` vars = do
+            uniqueName <- uniqueFuncName s
+            return [(uniqueName, concatMap collectArgs args)]
+      | otherwise = return []
+    collectApp _ _ = return []
 
     collectOp (QVarOp _ (UnQual _ (Ident _ s))) e1 e2
-      | s `notElem` vars = [(s, collectArgs e1 ++ collectArgs e2)]
-    collectOp _ _ _ = []
+      | s `notElem` vars = do
+            uniqueName <- uniqueFuncName s
+            return [(uniqueName, collectArgs e1 ++ collectArgs e2)]
+      | otherwise = return []
+    collectOp _ _ _ = return []
 
-    collectArgs (Var _ (UnQual _ (Ident _ v))) = [v]
+    collectArgs (Var _ (UnQual _ (Ident _ v))) = ["v_" ++ v]
     collectArgs (Paren _ e) = collectArgs e
     collectArgs _ = []
+
+uniqueFuncName :: String -> State Int String
+uniqueFuncName name = do
+  counter <- get
+  put (counter + 1)
+  return $ name ++ show counter
 
 
 
