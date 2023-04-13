5b02fd549b40ec3e8529ea636765b3f5db15aba8
Denx <25338291+denx20@users.noreply.github.com>
2023-04-13 19:55:07 -0400

    not too much shrooms yet

--- DependencyGraph/queue.hs
+++ DependencyGraph/queue.hs
@@ -9,10 +9,11 @@ import Control.Distributed.Process.Backend.SimpleLocalnet
 import Control.Concurrent (threadDelay)
 import Control.Concurrent.MVar
 import Control.Concurrent.Async
-import Control.Monad (forM_, mapM_)
+import Control.Monad (forM_, mapM_, forever)
 import qualified Data.HashMap.Strict as HM
 import System.Environment (getArgs)
-import Data.List ((\\), nub)
+import Data.List ((\\), nub, delete)
+import System.Random (randomRIO)
 
 import GHC.Generics (Generic)
 import Data.Binary
@@ -21,109 +22,102 @@ import Text.Printf
 
 type DependencyGraph = [(String, [String])]
 
-data Message = Result Double
+data Message = Result String String
   deriving (Typeable, Generic)
 
 instance Binary Message
 
-data RemoteCall = RemoteCall (String, [String]) ProcessId
+data RemoteCall = RemoteCall String ProcessId
   deriving (Show, Typeable, Generic)
 
 instance Binary RemoteCall
 
 remoteCall :: RemoteCall -> Process ()
-remoteCall (RemoteCall _ masterPid) = do
-  let result = 1 -- TODO: this is a dummy remote call, need to change 
-  send masterPid (Result result)
+remoteCall (RemoteCall node masterPid) = do
+  result <- liftIO $ randomString 10
+  send masterPid (Result node result)
 
-
+randomString :: Int -> IO String
+randomString len = sequence [randomRIO ('a', 'z') | _ <- [1..len]]
 
 remotable ['remoteCall]
 
 myRemoteTable :: RemoteTable
 myRemoteTable = Main.__remoteTable initRemoteTable
 
-{-
-executeFunctionAsync :: MVar (HM.HashMap String Int) -> (String, [String]) -> Process ()
-executeFunctionAsync resultsVar edge = do
-  let closure = $(mkClosure 'remoteCall) (RemoteCall edge)
-  myPid <- getSelfPid
-  remoteNode <- getSelfNode
-  pid <- spawn remoteNode closure
-  send pid myPid
-  result <- expect :: Process Int
-  liftIO $ modifyMVar_ resultsVar $ \hm -> return $ HM.insert (fst edge) result hm
--}
-
-executeFunctionAsync :: ProcessId -> NodeId  -> (String, [String]) -> Process ()
-executeFunctionAsync masterId nodeId edge = do
-  let remoteCallClosure = ($(mkClosure 'remoteCall) (RemoteCall edge masterId))
-  liftIO $ print $ typeOf remoteCallClosure
+dispatchJob :: ProcessId -> NodeId  -> String -> Process ()
+dispatchJob masterId nodeId node = do
+  let remoteCallClosure = ($(mkClosure 'remoteCall) (RemoteCall node masterId))
   _ <- spawn nodeId remoteCallClosure
   liftIO $ putStrLn $ "Sent task to worker: " ++ show nodeId
 
-
--- Simple FIFO load balancing
-rotate :: Int -> [a] -> [a]
-rotate _ [] = []
-rotate n xs = zipWith const (drop n (cycle xs)) xs
+rotate :: [a] -> [a]
+rotate [] = []
+rotate xs = tail xs ++ [head xs]
 
 master :: Backend -> [NodeId] -> Process ()
 master backend nodes = do
   let dependencyGraph = [("A", ["B", "C"]), ("B", ["D"]), ("C", ["D"]), ("D", [])]
   let reversedGraph = reverseDependencyGraph dependencyGraph
-  let indegreeZeroNodes = findIndegreeZeroNodes reversedGraph
+  let indegreeCount = [(v, length vs) | (v, vs) <- dependencyGraph]
+  let indegreeZeroNodes = map (\(node, cnt) -> node) $ filter (\(v, indegree) -> indegree == 0) indegreeCount
+
+  nodeListVar <- liftIO $ newMVar nodes
+  queueVar <- liftIO $ newMVar indegreeZeroNodes
+  indegreeMapVar <- liftIO $ newMVar $ HM.fromList indegreeCount
 
   say "Master started"
   say $ "Master discovered workers: " ++ (show nodes)
   masterPid <- getSelfPid
 
-  say "indegree 0 nodes:"
-  mapM_ say indegreeZeroNodes
-  say ""
+  queue <- liftIO $ readMVar queueVar
+  say $ "queue " ++ (show queue)
+  indegreeMap <- liftIO $ readMVar indegreeMapVar
+  say $ "indegreeMap " ++ (show indegreeMap)
 
-  resultsVar <- liftIO $ newMVar HM.empty
-  nodeListVar <- liftIO $ newMVar nodes
-  runningJobsList <- liftIO $ newMVar
+  let assignJobs = do
+          queue <- liftIO $ readMVar queueVar :: Process [String]
+          if null queue
+            then return ()
+            else do
+              nodeList <- liftIO $ readMVar nodeListVar :: Process [NodeId]
+              say $ "got node list: " ++ (show nodeList)
+              let (job, newQueue) = (head queue, tail queue)
+              let worker = head nodeList
+              liftIO $ modifyMVar_ queueVar $ \_ -> return newQueue
+              liftIO $ modifyMVar_ nodeListVar $ \_ -> return $ rotate nodeList
+              dispatchJob masterPid worker job
+              assignJobs
+          
+  -- let processReply = do
+  --       Result node result <- expect
+  --       liftIO $ putStrLn $ "Received result: " ++ node ++ " -> " ++ result
+  --       modifyMVar_ indegreeMapVar $ \indegrees ->
+  --         let updatedIndegrees = foldr (updateIndegreeMap node) indegrees (reversedGraph !! (HM.lookupDefault (-1) node indegrees))
+  --         in return $! updatedIndegrees
+  --       modifyMVar_ queueVar $ \queue ->
+  --         let newNodes = [v | (v, indegree) <- HM.toList updatedIndegrees, indegree == 0, v `notElem` queue]
+  --         in return $ queue ++ newNodes
+  --       processReply
 
-  forM_ indegreeZeroNodes $ \node -> do
-    let dependencyEdges = [(v, deps) | (v, deps) <- reversedGraph, node `elem` deps]
-    -- Send their action to the nodes
-    forM_ dependencyEdges $ \edge -> do
-      currentList <- liftIO $ takeMVar nodeListVar
-      executeFunctionAsync masterPid (head currentList) edge
-      let rotatedList = rotate 1 currentList
-      liftIO $ putMVar nodeListVar rotatedList
+  -- async processReply
+  forever assignJobs
 
-  resultMap <- liftIO $ readMVar resultsVar
-  say $ "Execution results: " ++ show resultMap
   liftIO $ threadDelay (200 * (10^6))
   terminateAllSlaves backend
 
--- Busy loop keeping the master running
-masterLoop :: ()
-
-slave :: Process ()
-slave = do
-  -- remoteCallClosure <- expect
-  -- liftIO $ print $ typeOf remoteCallClosure
-  -- result <- call remoteCallClosure
-  -- from <- expect :: Process ProcessId
-  -- send from result
-  slave
-
+updateIndegreeMap :: String -> String -> HM.HashMap String Int -> HM.HashMap String Int
+updateIndegreeMap node v indegrees =
+  HM.adjust (\indegree -> indegree - 1) v indegrees
 
 reverseDependencyGraph :: DependencyGraph -> DependencyGraph
 reverseDependencyGraph g = concatMap reverseEdges g
   where
     reverseEdges (v, vs) = [(v', [v]) | v' <- vs]
 
-findIndegreeZeroNodes :: DependencyGraph -> [String]
-findIndegreeZeroNodes g =
-  let allNodes = nub [v | (v, _) <- g]
-      nodesWithEdges = nub $ concatMap snd g
-   in allNodes \\ nodesWithEdges
-
+-- Takes indegreeMap and returns list of nodes with indegree 0
+-- findIndegreeZeroNodes :: HashMap -> [String]
+-- findIndegreeZeroNodes indegreeMap = HM.keys $ HM.filter (== 0) indegreeMap
 
 main :: IO ()
 main = do
@@ -136,8 +130,3 @@ main = do
       backend <- initializeBackend host port myRemoteTable
       startSlave backend
     _ -> putStrLn "Invalid command. Use 'master <host> <port>' or 'slave <host> <port>'."
-
-
-
-
-
