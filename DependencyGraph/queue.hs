87a2928de979b595c5bee55dcb5ffc5a98409a88
Tony Wu <uutonywu@gmail.com>
2023-04-13 12:50:01 -0400

    queue.hs still crippled

--- DependencyGraph/queue.hs
+++ DependencyGraph/queue.hs
@@ -1,36 +1,97 @@
-{-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveDataTypeable #-}
 
-
-import Control.Concurrent.Async (Async, async, wait)
-import Control.Distributed.Process (Process, Closure, call, RemoteTable, liftIO)
-import Control.Distributed.Process.Closure (mkClosure, remotable)
-import Control.Distributed.Process.Node (initRemoteTable, runProcess, newLocalNode)
-import Control.Distributed.Process.Backend.SimpleLocalnet (initializeBackend)
-import Control.Monad (void, forM_)
-import Data.Typeable (Typeable)
-import Data.List ((\\), nub)
-import Data.Binary 
-import qualified Data.HashMap.Strict as HM
+import Control.Distributed.Process hiding (Message)
+import Control.Distributed.Process.Closure
+import Control.Distributed.Process.Node (initRemoteTable)
+import Control.Distributed.Process.Backend.SimpleLocalnet
+import Control.Concurrent (threadDelay)
 import Control.Concurrent.MVar
+import Control.Concurrent.Async
+import Control.Monad (forM_)
+import qualified Data.HashMap.Strict as HM
+import System.Environment (getArgs)
+import Data.List ((\\), nub)
 
--- Add necessary imports
-import Control.Distributed.Process.Serializable (Serializable)
 import GHC.Generics (Generic)
+import Data.Binary
+import Data.Typeable
+import Text.Printf
 
 type DependencyGraph = [(String, [String])]
 
-data RemoteCall = RemoteCall (String, [String])
+data Message = Result Double
+  deriving (Typeable, Generic)
+
+instance Binary Message
+
+data RemoteCall = RemoteCall (String, [String]) ProcessId
   deriving (Show, Typeable, Generic)
 
 instance Binary RemoteCall
 
-remoteCall :: RemoteCall -> Process Float
-remoteCall (RemoteCall _) = return 1.0 -- TODO: This is dummy, need to change. RemoteCall should return remote worker's execution output
+remoteCall :: RemoteCall -> Process ()
+remoteCall (RemoteCall _ masterPid) = do
+  let result = 1 -- TODO: this is a dummy remote call, need to change 
+  send masterPid (Result result)
 
 
-reverseDependencyGraph :: DependencyGraph -> DependencyGraph  
+
+remotable ['remoteCall]
+
+myRemoteTable :: RemoteTable
+myRemoteTable = Main.__remoteTable initRemoteTable
+
+{-
+executeFunctionAsync :: MVar (HM.HashMap String Int) -> (String, [String]) -> Process ()
+executeFunctionAsync resultsVar edge = do
+  let closure = $(mkClosure 'remoteCall) (RemoteCall edge)
+  myPid <- getSelfPid
+  remoteNode <- getSelfNode
+  pid <- spawn remoteNode closure
+  send pid myPid
+  result <- expect :: Process Int
+  liftIO $ modifyMVar_ resultsVar $ \hm -> return $ HM.insert (fst edge) result hm
+-}
+
+executeFunctionAsync :: Int -> MVar (HM.HashMap String Int) -> (String, [String]) -> Process (Async ())
+executeFunctionAsync workerId resultsVar edge = do
+  let remoteCallClosure = ($(mkClosure 'remoteCall) (RemoteCall edge))
+  remoteNode <- liftIO $ findRemoteNode workerId
+  async $ do
+    spawn remoteNode remoteCallClosure
+    liftIO $ putStrLn $ "Sent task to worker: " ++ show workerId
+
+
+master :: Backend -> [NodeId] -> Process ()
+master backend nodes = do
+  let dependencyGraph = [("A", ["B", "C"]), ("B", ["D"]), ("C", ["D"]), ("D", [])]
+  let reversedGraph = reverseDependencyGraph dependencyGraph
+  let indegreeZeroNodes = findIndegreeZeroNodes reversedGraph
+
+  resultsVar <- liftIO $ newMVar HM.empty
+
+  forM_ indegreeZeroNodes $ \node -> do
+    let dependencyEdges = [(v, deps) | (v, deps) <- reversedGraph, node `elem` deps]
+    forM_ dependencyEdges $ \edge -> do
+      executeFunctionAsync resultsVar edge
+
+  resultMap <- liftIO $ readMVar resultsVar
+  say $ "Execution results: " ++ show resultMap
+  liftIO $ threadDelay (200 * (10^6))
+  terminateAllSlaves backend
+
+slave :: Process ()
+slave = do
+  remoteCallClosure <- expect
+  result <- call remoteCallClosure
+  from <- expect :: Process ProcessId
+  send from result
+  slave
+
+
+reverseDependencyGraph :: DependencyGraph -> DependencyGraph
 reverseDependencyGraph g = concatMap reverseEdges g
   where
     reverseEdges (v, vs) = [(v', [v]) | v' <- vs]
@@ -42,51 +103,19 @@ findIndegreeZeroNodes g =
    in allNodes \\ nodesWithEdges
 
 
-remotable ['remoteCall]
-
--- TODO: executeFunctionAsync is still buggy
-executeFunctionAsync :: Int -> MVar (HM.HashMap String Int) -> (String, [String]) -> Process (Async Int)
-executeFunctionAsync workerId resultsVar edge = do
-  let closure = mkClosure 'remoteCall (RemoteCall edge)
-  async $ do
-    result <- call closure
-    liftIO $ modifyMVar_ resultsVar $ \hm -> return $ HM.insert (fst edge) result hm
-    return result
-
--- TODO: executeFunctions this is still buggy
-executeFunctions :: RemoteTable -> [String] -> DependencyGraph -> IO (HM.HashMap String Int)
-executeFunctions rtable indegreeZeroNodes depGraph = do
-  resultsVar <- newMVar HM.empty
-  workerPool <- newMVar [1..10] -- We have 10 remote workers
-  let master = do
-        forM_ indegreeZeroNodes $ \node -> do
-          let dependencyEdges = [(v, deps) | (v, deps) <- depGraph, node `elem` deps]
-          forM_ dependencyEdges $ \edge -> do
-            workerId <- liftIO $ takeMVar workerPool
-            asyncHandle <- executeFunctionAsync workerId resultsVar edge
-            result <- wait asyncHandle
-            let dependent = fst edge
-            liftIO $ modifyMVar_ resultsVar $ \hm -> return $ HM.insert dependent result hm
-            liftIO $ putStrLn $ "Received result for " ++ dependent ++ ": " ++ show result
-            liftIO $ putMVar workerPool workerId
-  runProcessWithRTS rtable master
-  readMVar resultsVar
-
-
 main :: IO ()
 main = do
-  let dependencyGraph = [("A", ["B", "C"]), ("B", ["D"]), ("C", ["D"]), ("D", [])]
-  let reversedGraph = reverseDependencyGraph dependencyGraph
-  let indegreeZeroNodes = findIndegreeZeroNodes reversedGraph
-  putStrLn $ "Reversed Graph: " ++ show reversedGraph
-  putStrLn $ "Nodes with in-degree 0: " ++ show indegreeZeroNodes
+  args <- getArgs
+  case args of
+    ["master", host, port] -> do
+      backend <- initializeBackend host port myRemoteTable
+      startMaster backend (master backend)
+    ["slave", host, port] -> do
+      backend <- initializeBackend host port myRemoteTable
+      startSlave backend
+    _ -> putStrLn "Invalid command. Use 'master <host> <port>' or 'slave <host> <port>'."
 
-  backend <- initializeBackend "localhost" "8080" rtable
-  node <- newLocalNode backend
-  resultMap <- runProcess node $ executeFunctions (__remoteTable initRemoteTable) indegreeZeroNodes reversedGraph
-  putStrLn $ "Execution results: " ++ show resultMap
 
-  where
-    rtable :: RemoteTable
-    rtable = __remoteTable initRemoteTable
+
+
 
