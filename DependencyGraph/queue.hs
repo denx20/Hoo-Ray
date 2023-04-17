701c0067a4300adde4fa431cbd7812979bca9354
Yuxi Long <longyuxi@live.com>
2023-04-16 21:17:23 -0400

    removed need for async (dangerous)

--- DependencyGraph/queue.hs
+++ DependencyGraph/queue.hs
@@ -9,7 +9,7 @@ import Control.Distributed.Process.Backend.SimpleLocalnet
 import Control.Concurrent (threadDelay)
 import Control.Concurrent.MVar
 import Control.Concurrent.Async (race_)
-import Control.Distributed.Process.Async 
+-- import Control.Distributed.Process.Async 
 import Control.Monad (forM_, mapM_, forever, replicateM_, replicateM)
 import qualified Data.HashMap.Strict as HM
 import qualified Data.HashSet as HS
@@ -189,7 +189,8 @@ master backend nodes = do
               say "No reply"
               processReply
 
-  assignJobsAsync <- async (AsyncTask assignJobs)
+  -- assignJobsAsync <- async (AsyncTask assignJobs)
+  assignJobsAsync <- spawnLocal assignJobs
   processReply
   terminateAllSlaves backend
 
