2b643535fe5e42f9fe59c6348d724e7af08f40e6
Dennis Xu <dennisxu@pop-os.localdomain>
2023-05-21 01:54:45 -0400

    Refactor to use stack

---
+++ src/MatMul.hs
@@ -0,0 +1,111 @@
+{-# OPTIONS -fglasgow-exts #-}
+module MatMul
+  ( generateRandomMatrix,
+    matrixBenchmark,
+    mmult,
+    getFirstElement,
+    sumMatrix,
+    calculateMatrix,
+    build,
+    extractMiddle,
+    serializeDouble,
+    deserializeDouble,
+    serializeDoubleList,
+    deserializeDoubleList,
+  )
+where
+
+import Control.Exception
+import Control.Monad (replicateM)
+import Control.Monad.ST (ST, runST)
+import Data.Char (isDigit)
+import Data.List
+import Data.STRef (STRef, modifySTRef', newSTRef, readSTRef)
+import Data.Typeable
+import System.CPUTime
+import System.Random (StdGen, mkStdGen, randomRs)
+import Text.Printf
+
+class BuildList a r | r -> a where
+  build' :: [a] -> a -> r
+
+instance BuildList a [a] where
+  build' l x = reverse $ x : l
+
+instance BuildList a r => BuildList a (a -> r) where
+  build' l x y = build' (x : l) y
+
+build :: forall r a. (BuildList a r) => a -> r
+build x = build' [] x
+
+{-
+Define matrix operations and helper functions for the large matrix workload evaluation
+-}
+mmult :: Num a => [[a]] -> [[a]] -> [[a]]
+mmult a b = [[sum $ zipWith (*) ar bc | bc <- (transpose b)] | ar <- a]
+
+matrixBenchmark :: Int -> Int -> Int -> Double -> Int -> IO ()
+matrixBenchmark m n p range seed = do
+  start <- getCPUTime
+  let a = generateRandomMatrix m n range seed
+  let b = generateRandomMatrix n p range seed
+  let x = mmult a b
+  print (getFirstElement x)
+  end <- getCPUTime
+  let diff = fromIntegral (end - start) / (10 ^ 12 :: Double)
+  printf "m: %d, n: %d, p: %d. Execution time: %0.6f sec" m n p (diff :: Double)
+
+generateRandomMatrix :: Int -> Int -> Double -> Int -> [[Double]]
+generateRandomMatrix m n range seed =
+  let gen = mkStdGen seed
+      randomsList = randomRs (- range, range) gen
+   in chunksOf n $ take (m * n) randomsList
+
+chunksOf :: Int -> [a] -> [[a]]
+chunksOf _ [] = []
+chunksOf n xs = take n xs : chunksOf n (drop n xs)
+
+getFirstElement :: [[a]] -> a
+getFirstElement x = head (head x)
+
+sumMatrix :: [[Double]] -> Double
+sumMatrix x = sum (map sum x)
+
+calculateMatrix :: Double -> Double -> Double -> Double -> Double -> Double -> Double
+calculateMatrix m n p seedA seedB range = sumMatrix (mmult a b)
+  where
+    m' = round m
+    n' = round n
+    p' = round p
+    seedA' = round seedA
+    seedB' = round seedB
+    a = generateRandomMatrix m' n' range seedA'
+    b = generateRandomMatrix n' p' range seedB'
+
+extractMiddle :: String -> String
+extractMiddle ('f' : '_' : rest) = fst $ span (not . isDigit) rest
+extractMiddle _ = error "Invalid input format"
+
+readDouble :: String -> Double
+readDouble s = read s :: Double
+
+serializeDouble :: Double -> String
+serializeDouble x = show x
+
+deserializeDouble :: String -> Double
+deserializeDouble = readDouble
+
+serializeDoubleList :: [[Double]] -> String
+serializeDoubleList xs = intercalate "\n" $ map (intercalate "," . map show) xs
+
+deserializeDoubleList :: String -> [[Double]]
+deserializeDoubleList s = map (map read . splitOn ',') $ lines s
+
+splitOn :: Char -> String -> [String]
+splitOn delimiter = foldr (\c acc -> if c == delimiter then "" : acc else (c : head acc) : tail acc) [""]
+
+main :: IO ()
+main = do
+  matrixBenchmark 10000 5000 10000 100 512
+
+-- let x = build 1 2 3 4 5 :: [Int]
---
+++ test/matmul_fine_test.hs
@@ -0,0 +1,47 @@
+import MatMul
+import Prelude
+import Data.Time.Clock
+
+main :: IO ()
+main = do
+  let a1 = generateRandomMatrix 100 100 10.0 806593
+  let b1 = generateRandomMatrix 100 1000 10.0 145769
+  let c1 = mmult a1 b1
+  let tmp1 = sumMatrix c1
+  let a2 = generateRandomMatrix 100 100 10.0 595865
+  let b2 = generateRandomMatrix 100 1000 10.0 537770
+  let c2 = mmult a2 b2
+  let tmp2 = sumMatrix c2
+  let a3 = generateRandomMatrix 100 100 10.0 138029
+  let b3 = generateRandomMatrix 100 1000 10.0 596144
+  let c3 = mmult a3 b3
+  let tmp3 = sumMatrix c3
+  let a4 = generateRandomMatrix 100 100 10.0 687392
+  let b4 = generateRandomMatrix 100 1000 10.0 142194
+  let c4 = mmult a4 b4
+  let tmp4 = sumMatrix c4
+  let a5 = generateRandomMatrix 100 100 10.0 223510
+  let b5 = generateRandomMatrix 100 1000 10.0 229806
+  let c5 = mmult a5 b5
+  let tmp5 = sumMatrix c5
+  let a6 = generateRandomMatrix 100 100 10.0 84474
+  let b6 = generateRandomMatrix 100 1000 10.0 845663
+  let c6 = mmult a6 b6
+  let tmp6 = sumMatrix c6
+  let a7 = generateRandomMatrix 100 100 10.0 54310
+  let b7 = generateRandomMatrix 100 1000 10.0 132428
+  let c7 = mmult a7 b7
+  let tmp7 = sumMatrix c7
+  let a8 = generateRandomMatrix 100 100 10.0 626498
+  let b8 = generateRandomMatrix 100 1000 10.0 401610
+  let c8 = mmult a8 b8
+  let tmp8 = sumMatrix c8
+  let a9 = generateRandomMatrix 100 100 10.0 158360
+  let b9 = generateRandomMatrix 100 1000 10.0 22547
+  let c9 = mmult a9 b9
+  let tmp9 = sumMatrix c9
+  let a10 = generateRandomMatrix 100 100 10.0 234323
+  let b10 = generateRandomMatrix 100 1000 10.0 572796
+  let c10 = mmult a10 b10
+  let tmp10 = sumMatrix c10
+  print "Done"
