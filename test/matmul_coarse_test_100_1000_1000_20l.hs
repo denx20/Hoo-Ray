1245e0a33a962c6013574994965994f8d6bd379a
Yuxi Long <longyuxi@live.com>
2023-07-16 08:50:23 -0400

    commit test file

---
+++ test/matmul_coarse_test_100_1000_1000_20l.hs
@@ -0,0 +1,26 @@
+import MatMul
+import Control.Parallel
+
+main :: IO ()
+main = do
+  let tmp1 = calculateMatrix 100 1000 1000 806593 145769 1
+  let tmp2 = calculateMatrix 100 1000 1000 595865 537770 1
+  let tmp3 = calculateMatrix 100 1000 1000 138029 596144 1
+  let tmp4 = calculateMatrix 100 1000 1000 687392 142194 1
+  let tmp5 = calculateMatrix 100 1000 1000 223510 229806 1
+  let tmp6 = calculateMatrix 100 1000 1000 84474 845663 1
+  let tmp7 = calculateMatrix 100 1000 1000 54310 132428 1
+  let tmp8 = calculateMatrix 100 1000 1000 626498 401610 1
+  let tmp9 = calculateMatrix 100 1000 1000 158360 22547 1
+  let tmp10 = calculateMatrix 100 1000 1000 234323 572796 1
+  let tmp11 = calculateMatrix 100 1000 1000 940663 329744 1
+  let tmp12 = calculateMatrix 100 1000 1000 51328 8836 1
+  let tmp13 = calculateMatrix 100 1000 1000 297974 456964 1
+  let tmp14 = calculateMatrix 100 1000 1000 818954 654777 1
+  let tmp15 = calculateMatrix 100 1000 1000 852300 904853 1
+  let tmp16 = calculateMatrix 100 1000 1000 270362 194170 1
+  let tmp17 = calculateMatrix 100 1000 1000 222640 904925 1
+  let tmp18 = calculateMatrix 100 1000 1000 563832 472819 1
+  let tmp19 = calculateMatrix 100 1000 1000 142188 990064 1
+  let tmp20 = calculateMatrix 100 1000 1000 791772 114376 1
+  print "Done"
